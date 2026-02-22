7bd3a7ea9ef466d482ebf7bdab850d9ba0bc6431
ugouhar <ushran987@gmail.com>
2026-02-22 23:17:08 +0530

    Preventing accidental change of cached data by spread

--- client/src/hooks/useCache.tsx
+++ client/src/hooks/useCache.tsx
@@ -5,11 +5,12 @@ const cacheMap = new Map<string, Item[]>();
 
 export const useCache = () => {
   const getCachedData = useCallback((query: string) => {
-    return cacheMap.get(query);
+    const cached = cacheMap.get(query);
+    return cached ? [...cached] : undefined;
   }, []);
 
   const setCachedData = useCallback((query: string, response: Item[]) => {
-    cacheMap.set(query, response);
+    cacheMap.set(query, [...response]);
   }, []);
 
   return [getCachedData, setCachedData] as const;
